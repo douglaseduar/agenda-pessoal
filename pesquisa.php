2af6212f33e0874e3b176861854898cb56960c5f
douglaseduar <douglas060401@gmail.com>
2021-08-27 22:50:26 -0300

    feat: adicionar rodapé

--- pesquisa.php
+++ pesquisa.php
@@ -12,6 +12,7 @@ include_once("config/conexao.php");
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Lista de Tarefas</title>
 </head>
 <body>
@@ -96,9 +97,7 @@ include_once("config/conexao.php");
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
             </body>
+            <footer class="footer cadastro">
+<a href="https://instagram.com/douglaseduar"><h3>Douglas</h3></a>
+</footer>
     </html>
-<?php 
-
-
-
-?>
